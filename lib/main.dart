62032068ae5bc1684cecf2bb51e4dced4c1119b5
shubhh123 <shubhh1181@gmail.com>
2024-05-09 22:22:15 +0530

    code files updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,42 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget{
+
+  // GradientContainer( {key} ) {     //Here {} having inside are called as named parameters
+  //   //doing some initialization work
+  // }
+
+  // GradientContainer({key}) : super(key: key)     //Here {} having inside are called as named parameters
+    //doing some initialization work
+  const GradientContainer({super.key});
+  
+  /*context contains metadata about some useful information about this widget in the overall widget tree */
+  @override
+  Widget build(context) { //build automatically receives a value for the context parameter
+      return Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
                 Color.fromARGB(255, 203, 6, 238),
                 Color.fromARGB(255, 2, 25, 43)
               ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
           ),
+
           child: const Center(
-            child: Text('Hello world'),
+            child: Text('Hello world', style: TextStyle(color: Colors.amber, fontSize: 28.0),),
           ),
-        ),
-      ),
-    ),
-  );
+        );
+  }
 }
